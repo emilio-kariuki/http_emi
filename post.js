12e93172db7413562f52b04f1587b7e92affe8a5
emilio <emilio113kariuki@gmail.com>
2022-02-20 19:23:21 +0300

    February 20, 2022 7:23 PM

--- post.js
+++ post.js
@@ -1,20 +1,19 @@
-const {Client} = require('pg');
+const { Client } = require('pg');
 
 const client = new Client({
     host: "localhost",
     port: 5432,
     user: "postgres",
     password: "postgres",
-    database: "httpEmilio",
+    database: "postgres",
 });
-client.on("connect", ()=>{
-    console.log("Connection ended")
-} );
-client.on("end", ()=>{
-    console.log("Connection ended")
+client.on('connect', ()=>{
+    console.log('connection started');
 });
+client.on("end",() =>{
+    console.log("connection finished");
+})
 
-// client.listen(5000,()=>{console.log("server Started")});
 client.connect();
 
 module.exports = client;
