f34aeed1564a052d9b2544a6e697b504a0f7750f
emilio <emilio113kariuki@gmail.com>
2022-02-20 19:22:38 +0300

    February 20, 2022 7:22 PM

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { json } = require('express/lib/response');
 const app = express();
 
-app.use(json.express({extended:false}));
+app.use(express.json({extended:false}));
 
 app.get('/',(req,res)=>{
     res.send("Emilio is the best in the world");
