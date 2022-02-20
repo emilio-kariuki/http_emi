bdb0ca18e2df42c7740afe9363449f1e984a248c
emilio <emilio113kariuki@gmail.com>
2022-02-20 20:56:21 +0300

    February 20, 2022 8:56 PM

--- lib/main.dart
+++ lib/main.dart
@@ -77,7 +77,7 @@ class _HomeState extends State<Home> {
     body: jsonEncode({
           "first_name": "first_name.text",
           "second_name": "second_name.text",
-          "age": int.parse("age.text")
+          // "age": int.parse("age.text")
           
 })
     );
