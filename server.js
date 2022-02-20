4fb86ce663b552571ed315eee840cef342cca148
emilio <emilio113kariuki@gmail.com>
2022-02-20 19:27:20 +0300

    February 20, 2022 7:27 PM

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const { json } = require('express/lib/response');
 const app = express();
-
+const client = require('./post')
 app.use(express.json({extended:false}));
 
-app.post('/',(req,res)=>{
-    res.send("Emilio is the best in the world");
+app.post('/',async(req,res)=>{
+    try{
+        const {first_name,second_name,age} = req.body;
+        const dbt = client.query("INSERT INTO emilio(")
+
+    }catch(err){
+        console.log(err.message)
+    }
 })
 
 app.listen(5000,()=>{console.log("server started......")})
