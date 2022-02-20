717fe9e224fc18c049147a71902658e38a713c1f
emilio <emilio113kariuki@gmail.com>
2022-02-20 19:14:09 +0300

    February 20, 2022 7:14 PM

--- post.js
+++ post.js
@@ -1,2 +1,16 @@
 const {Client} = require('ps');
 
+const client = new Client({
+    host: "localhost",
+    port: 5432,
+    user: "postgres",
+    password: "postgres",
+    database: "postgres",
+});
+
+client.on("end", ()=>{
+    console.log("Connection ended")
+});
+client.on("on", ()=>{
+    console.log("Connection ended")
+} );
