f0544b52dd24e7d43e6017edfda8849329aec11d
emilio <emilio113kariuki@gmail.com>
2022-02-20 19:28:45 +0300

    February 20, 2022 7:28 PM

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ app.use(express.json({extended:false}));
 app.post('/',async(req,res)=>{
     try{
         const {first_name,second_name,age} = req.body;
-        const dbt = client.query("INSERT INTO emilio(")
+        const dbt = client.query("INSERT INTO emilio(first_name,second_name,age)VALUES($1,$2,$3)",[first_name,second_name,age]);
+        res.json(dbt);
 
     }catch(err){
         console.log(err.message)
