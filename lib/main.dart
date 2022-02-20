b9a10789b8a6cb9f79fe1b7e69469f7d83cca917
emilio <emilio113kariuki@gmail.com>
2022-02-20 20:28:17 +0300

    February 20, 2022 8:28 PM

--- lib/main.dart
+++ lib/main.dart
@@ -15,14 +15,72 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  final first_name = TextEditingController();
+  final second_name = TextEditingController();
+  final age = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('http')),
-      body: Center(child: ElevatedButton(
-        onPressed: (){},
-        child: Text("Press me"),
-    )));
+        appBar: AppBar(title: Text('http')),
+        body: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.fromLTRB(30, 0, 30, 0),
+              child: TextFormField(
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30.0),
+                    ),
+                    // icon: Icon(Icons.person,size:30,color: Colors.black),
+                    filled: true,
+                    hintStyle: TextStyle(color: Colors.grey[800]),
+                    focusColor: Colors.red,
+                    hintText: "Email",
+                    prefixIcon: Icon(Icons.mail, color: Colors.blueGrey[900]),
+                    fillColor: Colors.grey[200]),
+                controller: first_name,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(30, 0, 30, 0),
+              child: TextFormField(
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30.0),
+                    ),
+                    // icon: Icon(Icons.person,size:30,color: Colors.black),
+                    filled: true,
+                    hintStyle: TextStyle(color: Colors.grey[800]),
+                    focusColor: Colors.red,
+                    hintText: "Email",
+                    prefixIcon: Icon(Icons.mail, color: Colors.blueGrey[900]),
+                    fillColor: Colors.grey[200]),
+                controller: second_name,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(30, 0, 30, 0),
+              child: TextFormField(
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30.0),
+                    ),
+                    // icon: Icon(Icons.person,size:30,color: Colors.black),
+                    filled: true,
+                    hintStyle: TextStyle(color: Colors.grey[800]),
+                    focusColor: Colors.red,
+                    hintText: "Email",
+                    prefixIcon: Icon(Icons.mail, color: Colors.blueGrey[900]),
+                    fillColor: Colors.grey[200]),
+                controller: age,
+              ),
+            ),
+            Center(
+                child: ElevatedButton(
+              onPressed: () {},
+              child: Text("Press me"),
+            )),
+          ],
+        ));
   }
 }
-
